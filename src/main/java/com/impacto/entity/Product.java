eeaabfb0fac1be7e1af0a3449cdb82e156d71a46
Muhammad Haikal Ubaidillah Susanto <haikalsusanto21@gmail.com>
2024-09-14 01:49:17 +0700

    add id generation

--- src/main/java/com/impacto/entity/Product.java
+++ src/main/java/com/impacto/entity/Product.java
@@ -2,6 +2,8 @@ package com.impacto.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -14,6 +16,7 @@ import lombok.Setter;
 public class Product {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(nullable = false)
